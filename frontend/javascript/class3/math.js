f2c2e0d346a15661e9cee0157fe7b1bc38825a0c
Banita Das <banitaranidas8@gmail.com>
2024-10-08 11:48:55 -0400

    this is condition assignment

---
+++ frontend/javascript/class3/math.js
@@ -0,0 +1,29 @@
+let num1 = 5;
+let num2 = 34;
+let result = num2 + num1;
+// let result = num2 - num1;
+// let result = num2 * num1;
+// let result = num2 / num1;
+console.log(result);
+//modulo operator
+let a = 29;
+let final = a % 2;
+console.log(final);
+//expontial operator
+let num3 = 2 ** 4;
+console.log(num3);
+//relational opertaor
+console.log(10 > !5);
+console.log(5 === 5);
+console.log(5 !== "5");
+if (" ") {
+  //if we write any space then it will give true statemet
+  console.log("it is true");
+}
+let total_bill = 1000;
+let discount;
+if (total_bill >= 1000) {
+  console.log("you have got 10% discount");
+} else {
+  console.log("no discount sir/madam");
+}
