76dc2b10a46ef61bb54f7d12630bf4228634060d
Banita Das <banitaranidas8@gmail.com>
2024-10-21 12:39:01 -0400

    Task

---
+++ frontend/javascript/class5/ittiration.js
@@ -0,0 +1,43 @@
+// const readline = require("readline");
+// console.log(readline);
+// const r2 = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// let userAns;
+// function askNumber() {
+//   r2.question("What is the capital of Odisha :", (input1) => {
+//     userAns = input1;
+//     do {
+//       console.log("wrong ", userAns);
+//     } while (userAns !== bhubaneswar);
+//     {
+//       console.log("right", userAns);
+//     }
+//     r2.close();
+//   });
+// }
+// askNumber();
+// for loop
+// let i;
+// for (i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// let i;
+// for (i = 5; i >= 1; i--) {
+//   console.log(i);
+// }
+// let i;
+// let sum = 0;
+// for (i = 0; i < 5; i++) {
+//   let random = Math.floor(Math.random() * 10);
+//   let result = sum + random;
+//   console.log(`the random sum is ${result}`);
+// }
+
+for (let i = 0; i <= 5; i++) {
+  for (let j = 1; j <= 2; j++) {
+    console.log(j);
+  }
+  console.log(i);
+}
